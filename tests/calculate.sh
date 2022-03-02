6365f72c33ee3ccc7da29c16f0894332c6922e63
Gavin Peng <gpeng@oicr.on.ca>
2022-03-02 13:50:18 -0500

    modified calculate.sh

--- tests/calculate.sh
+++ tests/calculate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+[200~#!/bin/bash
 set -o nounset
 set -o errexit
 set -o pipefail
@@ -6,6 +6,7 @@ set -o pipefail
 #enter the workflow's final output directory ($1)
 cd $1
 
-#find all files, return their md5sums to std out
+#find all vcf and fin files, return their md5sums to std out, list all file types
 find . -name *.vcf.gz -xtype f -exec sh -c "zcat {} | grep -v ^# | md5sum" \;
+find . -name *.fin -xtype f -exec sh -c "cat {} | md5sum" \;
 ls | sed 's/.*\.//' | sort | uniq -c
